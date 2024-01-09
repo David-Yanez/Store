df5bfdf03ae2bd97b5b95b336f6fb56050cbaf08
David-Yanez <luis.yanez@epn.edu.ec>
2024-01-09 17:34:10 -0500

    Inicio del proyecto y creacion de paginas

---
+++ src/Pages/App/index.jsx
@@ -0,0 +1,19 @@
+import { Home } from '../Home'
+import { MyAccount } from '../MyAccount'
+import { MyOrder } from '../MyOrder'
+import { MyOrders } from '../MyOrders'
+import { NotFound } from '../NotFound'
+import { SignIn } from '../SignIn'
+
+export const App = () => {
+  return (
+    <>
+      <Home />
+      <MyAccount />
+      <MyOrder />
+      <MyOrders />
+      <NotFound />
+      <SignIn />
+    </>
+  )
+}
