579e0bd190e97b4f426c20f924afc29944c543fe
David-Yanez <luis.yanez@epn.edu.ec>
2024-01-15 23:45:19 -0500

    Creacción contexto

---
+++ src/Context/index.jsx
@@ -0,0 +1,12 @@
+import { createContext } from 'react'
+
+const ShoopingCartContext = createContext()
+
+export const ShoopingCartProvider = ({ children }) => {
+  return (
+    <ShoopingCartContext.Provider>
+      {children}
+    </ShoopingCartContext.Provider>
+
+  )
+}
