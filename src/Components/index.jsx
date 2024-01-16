5d6b4fdacadf6c54a9ec11efb91ee8c9d4a8e2ce
David-Yanez <luis.yanez@epn.edu.ec>
2024-01-15 21:19:49 -0500

    layout agregado

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
   const activeStyle = 'underline underline-offset-4'
 
   return (
-    <nav className='flex justify-between items-center fixed z-10 w-full py-5 px-8 '>
+    <nav className='flex justify-between items-center fixed z-10 w-full py-5 px-8 top-0 '>
       <ul className='flex items-center gap-3'>
         <li className='font-semibold text-lg'>
           <NavLink to='/'>
