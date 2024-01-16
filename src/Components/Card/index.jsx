d82448005ac17619cdad738b49e5e2b9fc728b1a
David-Yanez <luis.yanez@epn.edu.ec>
2024-01-15 23:27:58 -0500

    Creacción cards y consumo API

---
+++ src/Components/Card/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const Card = (data) => {
+  return (
+
+    <div class='w-56 max-w-sm   bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+      <a href='#'>
+        <img class='p-4 rounded-t-lg  rounded-lg' src={data.data.images[0]} alt={data.data.title} />
+      </a>
+      <div class='px-5 pb-5'>
+        <a href='#'>
+          <h5 class='text-x font-semibold tracking-tight text-gray-900 dark:text-white'>{data.data.title}</h5>
+        </a>
+        <div class='flex items-center mt-2.5 mb-5'>
+          <span class='bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3'>{data.data.category.name}</span>
+        </div>
+        <div class='flex items-center justify-between'>
+          <span class='text-2xl font-bold text-gray-900 dark:text-white'>${data.data.price}</span>
+          <a href='#' class='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xs px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>Add to cart</a>
+        </div>
+      </div>
+    </div>
+
+  )
+}
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,8 +1,26 @@
+import { useState, useEffect } from 'react'
+import { Card } from '../../Components/Card/index'
+
 export const Home = () => {
+  const [items, setItems] = useState(null)
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, [])
+
   return (
 
-    <div>
-      Home
+    <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+      {
+        items?.map((item) => (
+          <Card key={item.id} data={item} />
+        )
+
+        )
+      }
+
     </div>
 
   )
